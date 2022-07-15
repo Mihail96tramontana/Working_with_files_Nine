40794c9225341ceba1fdfbb6f9d9e592c08bc98b
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2022-07-15 14:30:23 +0300

    test

--- src/test/java/guru/qa/FileParseTest.java
+++ src/test/java/guru/qa/FileParseTest.java
@@ -1,4 +1,45 @@
 package guru.qa;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class FileParseTest {
-}
+
+    @Test
+    void pdfTest() throws Exception {
+        ZipFile zipFiles = new ZipFile(zipClassPath);
+        ZipEntry zipEntry = zipFiles.getEntry("");
+        InputStream inputStream = zipFiles.getInputStream(zipEntry);
+        PDF pdf = new PDF(inputStream);
+        assertThat(pdf.text).contains("");
+    }
+
+    @Test
+    void xlsxTest() throws Exception {
+        ZipFile zipFiles = new ZipFile(zipClassPath);
+        ZipEntry zipEntry = zipFiles.getEntry("");
+        InputStream inputStream = zipFiles.getInputStream(zipEntry);
+        XLS xls = new XLS(inputStream);
+        assertThat(xls.excel
+                .getSheetAt()
+                .getRow()
+                .getCell()
+                .getStringCellValue()).contains("");
+    }
+
+    @Test
+    void parseCsvTest() throws Exception {
+        ZipFile zipFiles = new ZipFile(zipClassPath);
+        ZipEntry zipEntry = zipFiles.getEntry("");
+        try (InputStream inputStream = zipFiles.getInputStream(zipEntry);
+             CSVReader csv = new CSVReader(new InputStreamReader(inputStream))) {
+            List<String[]> content = csv.readAll();
+            assertThat(content.get()).contains("");
+        }
+    }
