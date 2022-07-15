56f57d0717ea1e480cb2e382a42262ede713ffce
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2022-07-15 16:46:34 +0300

    test

--- src/test/java/guru/qa/FileParseTest.java
+++ src/test/java/guru/qa/FileParseTest.java
@@ -2,17 +2,21 @@ package guru.qa;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileParseTest {
+
+    ClassLoader classLoader = FileParseTest.class.getClassLoader();
 
     String zipClassPath = "src/test/resources/Files.zip";
 
@@ -39,12 +43,20 @@ public class FileParseTest {
     }
 
     @Test
-    void parseCsvTest() throws Exception {
-        ZipFile zipFiles = new ZipFile(zipClassPath);
-        ZipEntry zipEntry = zipFiles.getEntry("Табличечка.csv");
-        try (InputStream inputStream = zipFiles.getInputStream(zipEntry);
-             CSVReader csv = new CSVReader(new InputStreamReader(inputStream))) {
-            List<String[]> content = csv.readAll();
-            assertThat(content.get()).contains("");
+    void csvTest() throws Exception {
+        //с помощью класс лоудера ищем зип
+        ZipFile zipFile = new ZipFile(Objects.requireNonNull(classLoader.getResource("Files.zip")).getFile());
+        //достаем нужный файл в зипе
+        ZipEntry entry = zipFile.getEntry("example.csv");
+        List<String[]> list;
+        InputStream inputStream = zipFile.getInputStream(entry);
+        //читаем csv
+        CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream));
+        list = csvReader.readAll();
+        assertThat(list).contains(
+                new String[] {"teacher","lesson","date"},
+                new String[] {"Tuchs","junit","03.06"},
+                new String[] {"Eroshenko","allure","07.06"}
+        );
         }
     }
